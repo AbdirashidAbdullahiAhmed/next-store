d00bae9cf331c7b4f964638132b2398df5844556
TaHa <madax361@gmail.com>
2024-09-30 14:04:15 +0300

    first commit

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,16 @@
+import { VscCode } from 'react-icons/vsc'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import React from 'react'
+
+function Logo() {
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <VscCode className="w-6 h-6" />
+      </Link>
+    </Button>
+  )
+}
+
+export default Logo
